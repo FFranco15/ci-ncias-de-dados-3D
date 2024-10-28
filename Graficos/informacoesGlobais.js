2ec14db73f41d56fca9b85e2f8f1c5b5cbb693b2
Franco <133654956+FFranco15@users.noreply.github.com>
2024-10-28 15:28:08 -0300

    aula 5_2°-2 correção

--- Graficos/informacoesGlobais.js
+++ Graficos/informacoesGlobais.js
@@ -3,7 +3,7 @@ const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    console.log(dados.tempo_medio);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
